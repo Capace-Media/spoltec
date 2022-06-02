6b62e87666ea22cee5c303c780c21402dc8785e8
capacemedia <developer@capace.se>
2022-06-02 09:11:20 +0200

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Blocks from '@common/components/Blocks';
 import MainHero from '@common/sections/MainHero';
 import PageTransition from '@modules/transitions/components/PageTransition';
 import getPage from '@modules/pages/lib/getPage';
+import Head from 'next/head';
 export const getStaticProps = async (context) => {
   const page = await getPage('/');
   return { 
@@ -15,7 +16,11 @@ export const getStaticProps = async (context) => {
 export default function Home({ page }) {
   return (
     <PageTransition>
+      
       <>
+        <Head>
+          <meta name="google-site-verification" content={process.env.VERIFICATION_ID} />
+        </Head>
         <MainHero />
         <Blocks blocks={page?.gqlBlocks?.blocks} />
       </>
