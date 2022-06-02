b60be4adbfa8607c00411bee522d981c8c4ab117
capacemedia <developer@capace.se>
2022-06-02 13:06:36 +0200

    added gtm & gtag.js

--- modules/blocks/components/Contact.tsx
+++ modules/blocks/components/Contact.tsx
@@ -14,19 +14,19 @@ const Contact = () => {
 
                     <div className="grid gap-5 md:grid-cols-3">
                         <div className="flex flex-col items-center justify-center">
-                            <a href="tel:040474012" className="flex flex-col items-center justify-center space-y-4 text-brand-blue">
+                            <a id="contact-tel" href="tel:040474012" className="flex flex-col items-center justify-center space-y-4 text-brand-blue">
                                 <Phone />
                                 <p className="font-semibold">040 - 47 40 12</p>
                             </a>
                         </div>
                         <div className="flex flex-col items-center justify-center space-y-4 text-brand-blue">
-                            <a href="mailto:info@spoltec.se" className="flex flex-col items-center justify-center space-y-4 text-brand-blue">
+                            <a id="contact-mail" href="mailto:info@spoltec.se" className="flex flex-col items-center justify-center space-y-4 text-brand-blue">
                                 <Mail />
                                 <p className="font-semibold">info@spoltec.se</p>
                             </a>
                         </div>
                         <div className="flex flex-col items-center justify-center space-y-4 text-brand-blue">
-                            <a target='_blank' rel='noreferrer' href="https://www.google.com/maps/place/Spoltec+S%C3%B6dra+AB/@55.8321596,13.32522,15z/data=!4m5!3m4!1s0x0:0x908ee730d7835c04!8m2!3d55.8321596!4d13.32522" className="flex flex-col items-center justify-center space-y-4 text-brand-blue">
+                            <a id="contact-address" target='_blank' rel='noreferrer' href="https://www.google.com/maps/place/Spoltec+S%C3%B6dra+AB/@55.8321596,13.32522,15z/data=!4m5!3m4!1s0x0:0x908ee730d7835c04!8m2!3d55.8321596!4d13.32522" className="flex flex-col items-center justify-center space-y-4 text-brand-blue">
                                 <Address />
                                 <p className="font-semibold text-center">Grävmaskinsvägen 2, <br />241 38 Eslöv</p>
                             </a>
