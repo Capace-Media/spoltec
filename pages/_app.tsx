39bafd7375103d956f012eec5db56fdbd656bc05
Cazpian Levén <cazpians@gmail.com>
2022-12-06 10:28:15 +0100

    Think I've found a solution to enchance gtag performance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -119,9 +119,12 @@ function MyApp({ Component, pageProps }) {
         }}
       />   */}
         
-          <Script 
+          {/* <Script 
             src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-          />
+          /> */}
+
+          <link href={`https://www.googletagmanager.com/gtm.js?id=${gtag.GA_TRACKING_ID}`} rel="preload" as="script"></link>
+
           <Script 
             id="gtag-init"
             strategy="afterInteractive"
@@ -160,21 +163,23 @@ function MyApp({ Component, pageProps }) {
       {/* Google Tag Manager - Global base code */}
       {process.env.NEXT_PUBLIC_GA_ID && (
 
-        // <Script
-        //   id="gtag-base"
-        //   strategy="afterInteractive"
-        //   dangerouslySetInnerHTML={{
-        //     __html: `
-        //       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        //       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        //       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        //       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        //       })(window,document,'script','dataLayer', '${GTM_ID}');
-        //     `,
-        //   }}
-        // />
+        <>
+          {/* <Script
+            id="gtag-base"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer', '${GTM_ID}');
+              `,
+            }}
+          /> */}
 
-        <link href={`https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`} rel="preload" as="script"></link>
+          <link href={`https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`} rel="preload" as="script"></link>
+        </>
       )}
 
       
