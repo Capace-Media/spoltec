d196ae351b08bf6f2687ceaf6b7d7f1298efa7e6
Rick Centerhall <developer@capace.se>
2024-07-03 13:36:41 +0200

    chore: fix googleanalytics and gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,6 @@
 import Layout from "@modules/layout/components/Layout";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { useRouter } from "next/router";
-import Script from "next/script";
-import { useEffect, useState } from "react";
-
-import * as gtag from "../lib/gtag";
-import { GTM_ID, pageview } from "../lib/gtm";
 import "../styles/global.scss";
 import "../styles/globals.css";
 
@@ -13,73 +9,8 @@ function MyApp({ Component, pageProps }) {
 
   const completeRoute = router.asPath;
 
-  useEffect(() => {
-    const handleRouteChange = (url: any) => {
-      gtag.pageview(url);
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-    router.events.on("hashChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-      router.events.off("hashChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
-  useEffect(() => {
-    router.events.on("routeChangeComplete", pageview);
-    return () => {
-      router.events.off("routeChangeComplete", pageview);
-    };
-  }, [router.events]);
-
   return (
     <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-
-      <>
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag() {
-                  dataLayer.push(arguments);
-                }
-                gtag('js', new Date());
-                gtag('config', '${gtag.GA_TRACKING_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-          }}
-        />
-      </>
-
-      {/* Google Tag Manager - Global base code */}
-      {process.env.NEXT_PUBLIC_GA_ID && (
-        <>
-          <Script
-            id="gtag-base"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer', '${GTM_ID}');
-              `,
-            }}
-          />
-          <link
-            href={`https://www.googletagmanager.com/gtm.js?id=${GTM_ID}`}
-            rel="preload"
-            as="script"
-          ></link>
-        </>
-      )}
-
       <Layout
         description={
           pageProps?.page?.gqlHeroFields?.introduktionstext ||
@@ -93,6 +24,8 @@ function MyApp({ Component, pageProps }) {
       >
         <Component {...pageProps} />
       </Layout>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID} />
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
     </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,10 @@
-import { Head, Html, Main, NextScript } from 'next/document';
-import { GTM_ID } from '../lib/gtm'
+import { Head, Html, Main, NextScript } from "next/document";
+
 export default function Document() {
   return (
-    <Html lang='sv'>
-      <Head>
-
-      </Head>
+    <Html lang="sv">
+      <Head></Head>
       <body>
-        {/* Google Tag Manager (noscript)  */}
-        <noscript>
-          <iframe 
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
         <Main />
         <NextScript />
       </body>
