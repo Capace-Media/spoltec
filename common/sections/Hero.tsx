651e9d8ded3f3cf48e906865cd61e90428162bff
juliabengtsson2 <julia.bengtsson2@hotmail.com>
2025-04-09 10:52:55 +0200

    added trustindex review to service pages

--- common/sections/Hero.tsx
+++ common/sections/Hero.tsx
@@ -1,8 +1,8 @@
-import parse from "html-react-parser";
-import Image from "next/image";
-import Link from "next/link";
-import Blocks from "../components/Blocks";
-import GoogleReviewsWidget from "components/GoogleReviews";
+import parse from 'html-react-parser';
+import Image from 'next/image';
+import Link from 'next/link';
+import Blocks from '../components/Blocks';
+import GoogleReviewsWidget from 'components/GoogleReviews';
 interface HeroProps {
   image?: string;
   title: string;
@@ -20,32 +20,29 @@ const Hero = ({ image, title, subtitle, text }: HeroProps) => {
               src={image}
               fill
               style={{
-                objectFit: "cover",
-                opacity: "0.4",
+                objectFit: 'cover',
+                opacity: '0.4',
               }}
               alt={title}
             />
           )}
 
-          <div className="flex items-center contain h-96">
+          <div className="flex items-center contain">
             <div className="max-w-lg text-white">
               <h1 className="text-white">{title}</h1>
               <strong className="block mb-3">{subtitle}</strong>
               {text && parse(text)}
-              <div className="flex flex-col gap-4">
-                <div>
-                  <Link
-                    className="inline-block mt-10 btn bg-brand-orange "
-                    href="/kontakta-oss"
-                  >
-                    Kontakta oss
-                  </Link>
-                </div>
-                <div className="flex flex-start">
-                  <GoogleReviewsWidget />
-                </div>
+              <div>
+                <Link
+                  className="inline-block mt-10 btn bg-brand-orange "
+                  href="/kontakta-oss">
+                  Kontakta oss
+                </Link>
               </div>
             </div>
+          </div>
+          <div className="flex pl-5 pt-3 md:justify-end md:pr-5 lg:pr-14">
+            <GoogleReviewsWidget />
           </div>
         </div>
       </section>
--- components/GoogleReviews.tsx
+++ components/GoogleReviews.tsx
@@ -1,32 +1,54 @@
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 const GoogleReviewsWidget = () => {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://cdn.trustindex.io/loader.js?6f2253e44dfe3429ba463bec87e';
-        script.async = true;
-        
-        document.body.appendChild(script);
-    
-        return () => {
-          if (script.parentNode) {
-            script.parentNode.removeChild(script);
-          }
-        };
-    }, []);
+  const router = useRouter();
+  const currentPage = router.asPath;
 
-    return (
-        <main>
-            <div id="trustindex-widget">
-                <iframe
-                    src="https://cdn.trustindex.io/loader.js?6f2253e44dfe3429ba463bec87e"
-                    width="100%"
-                    height="500px"
-                    title="Google Reviews"
-                />
-            </div>
-        </main>
-    );
+  const widgetPage = [
+    '/tjanster',
+    '/relining-malmo',
+    '/relining-helsingborg',
+    '/relining-vaxjo',
+    '/relining-boras',
+    '/relining-goteborg',
+    '/relining-halmstad',
+    '/relining-jonkoping',
+    '/relining-kristianstad',
+    '/relining-varberg'
+  ]
+
+  const servicePage = widgetPage.some(path => currentPage.includes(path));
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src =
+      'https://cdn.trustindex.io/loader.js?6f2253e44dfe3429ba463bec87e';
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      if (script.parentNode) {
+        script.parentNode.removeChild(script);
+      }
+    };
+  }, []);
+
+  return (
+    <main>
+      {servicePage && (
+        <div id="trustindex-widget">
+          <iframe
+            src="https://cdn.trustindex.io/loader.js?6f2253e44dfe3429ba463bec87e"
+            width="100%"
+            height="500px"
+            title="Google Reviews"
+          />
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default GoogleReviewsWidget;
