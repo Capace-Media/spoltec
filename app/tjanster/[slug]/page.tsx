221734f9cd0f8bfb26abc6098a29507b3fb53115
R C <rickoliwer@gmail.com>
2025-09-05 15:45:59 +0200

    optimize sitemap

--- app/tjanster/[slug]/page.tsx
+++ app/tjanster/[slug]/page.tsx
@@ -8,6 +8,7 @@ import { breadcrumbsSchema, serviceSchema } from "@lib/seo/schema";
 import JsonLd from "components/JsonLd";
 import { absoluteUrl } from "@lib/utils/url";
 import { fetchGraphQL } from "@lib/wp/fetchGraphQL";
+import BreadcrumbsComponent from "@components/breadcrumbs";
 
 export const dynamicParams = true;
 
@@ -76,22 +77,29 @@ export default async function ServicePage(props: PageProps) {
   }
 
   const canonical = await absoluteUrl(`/tjanster/${slug}`);
+
+  const breadcrumbItems = [
+    {
+      name: "Hem",
+      url: await absoluteUrl("/"),
+    },
+    {
+      name: "Tjänster",
+      url: await absoluteUrl("/tjanster"),
+    },
+    {
+      name: page?.title ?? "",
+      url: canonical,
+      type: "Service",
+      current: true,
+    },
+  ];
   const bread = breadcrumbsSchema(
-    [
-      {
-        name: "Hem",
-        url: await absoluteUrl("/"),
-      },
-      {
-        name: "Tjänster",
-        url: await absoluteUrl("/tjanster"),
-      },
-      {
-        name: page?.title ?? "",
-        url: canonical,
-        type: "Service",
-      },
-    ],
+    breadcrumbItems.map((item) => ({
+      name: item.name,
+      url: item.url,
+      type: item.current ? "Service" : undefined,
+    })),
     canonical
   );
   const serviceSchemaLD = serviceSchema(page);
@@ -108,7 +116,8 @@ export default async function ServicePage(props: PageProps) {
           image={page?.gqlHeroFields?.bild?.mediaItemUrl}
           usp={page?.gqlHeroFields.usp}
         />
-        <div id="content" className="w-full h-10 md:h-0"></div>
+        <BreadcrumbsComponent items={breadcrumbItems} />
+
         <div>
           <Blocks blocks={page?.gqlBlocks?.blocks || []} />
         </div>
