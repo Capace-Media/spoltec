98e0b0626132749e4bbcd2ab22b4a41ecafcb8a3
Darion Valdez <darionvldz@gmail.com>
2026-06-10 16:15:46 +0200

    add cookie declaration

---
+++ app/cookiepolicy/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useEffect } from "react";
+
+export default function Page() {
+  useEffect(() => {
+    const container = document.getElementById("CookiebotDeclaration");
+    if (!container) return;
+
+    container.innerHTML = "";
+
+    const script = document.createElement("script");
+    script.src =
+      "https://consent.cookiebot.com/6e90d32a-24e5-4ede-b5a5-f3650b321fe2/cd.js";
+    script.async = true;
+    container.appendChild(script);
+
+    return () => {
+      script.remove();
+      container.innerHTML = "";
+    };
+  }, []);
+
+  return (
+    <div className="max-w-300 mx-auto">
+      <h1>Cookiepolicy</h1>
+      <div id="CookiebotDeclaration" />
+    </div>
+  );
+}
